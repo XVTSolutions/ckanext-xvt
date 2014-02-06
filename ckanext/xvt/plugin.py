f889aa92cae40a2b903d567bd7fa7597823aeca4
george <george.sattler@xvt.com.au>
2014-02-06 20:51:24 +1100

    Initial commit

---
+++ ckanext/xvt/plugin.py
@@ -0,0 +1,10 @@
+import ckan.plugins as plugins
+
+
+class XVTPluginClass(plugins.SingletonPlugin):
+    """
+    Setup plugin
+    """
+    print '#############################'
+    print '#        ckanext-xvt        #'
+    print '#############################'
